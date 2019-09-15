5a3ffb1935c7c4cc0b33f7e07a0833bc08cb44e4
Mike Møller Nielsen <ekim197711@gmail.com>
2019-09-15 21:07:24 +0200

    files

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# maven-custom-plugin" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:ekim197711/maven-custom-plugin.git
+git push -u origin master
---
+++ src/main/java/com/codeinvestigator/WeatherForecastPlugin.java
@@ -0,0 +1,27 @@
+package com.codeinvestigator;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name="weatherforecastTomorrow", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+public class WeatherForecastPlugin extends AbstractMojo {
+
+    @Parameter(defaultValue = "NotSet")
+    private String forecast;
+
+    @Parameter(defaultValue = "${project}")
+    private MavenProject project;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("*** weatherforecastTomorrow plugin BEGIN ***");
+        getLog().info("The forecast for tomorrow is: " + forecast);
+        getLog().info("Project build output directory: " + project.getBuild().getOutputDirectory());
+        getLog().info("*** weatherforecastTomorrow plugin END ***");
+    }
+}
